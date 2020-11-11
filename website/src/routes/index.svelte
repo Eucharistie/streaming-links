bd9fae732fab766f36c0a9519ea4453e86c90a98
Damiaan Dufaux <damiaan@dpro.be>
2020-11-11 18:13:54 +0100

    Add homepage

--- website/src/routes/index.svelte
+++ website/src/routes/index.svelte
@@ -1,18 +1,39 @@
-<script context="module">
-	export async function preload() {
-		return { cool: process.cwd() }
-	}
-</script>
-
-<script>
-	export let cool;
-</script>
-
 <svelte:head>
 	<title>Eucharistie-info | Enkele links om de Eucharistie live te volgen.</title>
 </svelte:head>
 
-<main>
-	<h1>Hello world</h1>
-	<pre>{cool}</pre>
-</main>
+<div class="d-md-block hide-lg hide-xl slide-up" hidden id="SideNav">
+  <!-- TODO include sidenav.html -->
+</div>
+<div class="blockquote-wrapper">
+  <div class="blockquote">
+    <h1>
+      In deze pandemische situatie, worden we uitgenodigd om
+      <span style="color:#ffffff">de waarde van de communio te
+      herontdekken</span> en te verdiepen.
+    </h1>
+    <h4>&mdash;Paus Franciscus<br><em>Angelus, 15 Maart</em></h4>
+  </div>
+</div>
+<div class="container-lg clearfix limit-children">
+
+  <div class="col-12 col-sm-6 col-md-4 col-lg-3 float-left text-center">
+    <a href="/uurrooster">
+      <div class="bg-blue rounded-md-2 my-md-6 mx-md-3 p-4">
+        <h6 class="m-0 text-white">
+          Volg de eucharistie live:
+        </h6>
+        <h4 class="text-white mt-2">
+          Uurrooster
+        </h4>
+        <svg viewBox="0 0 120 120" class="logo avatar" style="width:140px;">
+          <use xlink:href="#logoCircle" class="white"/>
+          <use xlink:href="#logoShape" class="blue"/>
+        </svg>
+      </div>
+    </a>
+  </div>
+
+	<!-- TODO add H. Mass links -->
+
+</div>
