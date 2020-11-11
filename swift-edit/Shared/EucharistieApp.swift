1b0e8436744683e34c45044b019bfc32449f5054
Damiaan Dufaux <damiaan@me.cpom>
2020-11-11 10:29:00 +0100

    Add swift editor

---
+++ swift-edit/Shared/EucharistieApp.swift
@@ -0,0 +1,17 @@
+//
+//  EucharistieApp.swift
+//  Shared
+//
+//  Created by Damiaan Dufaux on 10/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct EucharistieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
